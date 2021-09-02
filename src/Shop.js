3a0e3d0baf979febbd963ac363e5e5bd44fd2fe2
Timilehin Arigbede <Harriemannie@gmail.com>
2021-09-02 14:41:39 +0100

    Fetch data from an external Api and store this data in the shop component

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Shop() {
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch('https://fortnite-api.com/v2/cosmetics/br');
+
+    const items = await data.json();
+    setItems(items.data);
+    console.log(items.data);
+  }
+
   return (
     <div>
-      <h1>Shop Page</h1>
+      {items.map(item => (
+        <h1 key={item.id}>{item.name}</h1>
+      ))}
     </div>
   );
 }
